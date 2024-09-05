4b6f53f254a772c282f03b45648f34a14e52bbd5
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-05 20:34:13 +0200

    Add new querie to find one videogame informations

--- db/queries.js
+++ db/queries.js
@@ -1,8 +1,15 @@
 const pool = require("./pool");
 
+async function getVideogameGenrePublisher(videogameName) {
+  const { rows } = await pool.query(
+    `SELECT videogame.id, videogame_name, videogame_description, videogame_price, videogame_release_date, videogame_rating, publisher,  STRING_AGG(videogame_categorie_name, ' ' ORDER BY videogame_categorie_name DESC) AS videogame_genre FROM videogame JOIN videogame_genre ON videogame.id = videogame_genre.id JOIN videogame_publisher ON videogame.id = videogame_publisher.id WHERE videogame_name = '${videogameName}' GROUP BY videogame.id, videogame_genre.id, videogame_publisher.publisher;`
+  );
+  return rows;
+}
+
 async function getAllVideogamesGenresPublishers() {
   const { rows } = await pool.query(
-    "SELECT videogame.id, videogame_name, videogame_description, videogame_price, videogame_release_date, videogame_rating, publisher,  STRING_AGG(videogame_categorie_name, ' ' ORDER BY videogame_categorie_name DESC) AS videogame_genre FROM videogame JOIN videogame_genre ON videogame.id = videogame_genre.id JOIN videogame_publisher ON videogame.id = videogame_publisher.id GROUP BY videogame.id, videogame_genre.id, videogame_publisher.publisher;"
+    "SELECT videogame.id, videogame_name, videogame_description, videogame_price, videogame_release_date, videogame_rating, publisher,  STRING_AGG(videogame_categorie_name, ' ' ORDER BY videogame_categorie_name DESC) AS videogame_genre FROM videogame JOIN videogame_genre ON videogame.id = videogame_genre.id JOIN videogame_publisher ON videogame.id = videogame_publisher.id  GROUP BY videogame.id, videogame_genre.id, videogame_publisher.publisher;"
   );
   return rows;
 }
@@ -62,6 +69,7 @@ async function insertNewVideogameCategorie(newVideogameCategorie) {
 }
 
 module.exports = {
+  getVideogameGenrePublisher,
   getAllVideogamesGenresPublishers,
   getAllVideogamesCategoriesAndDescriptions,
   getAllVideogamesCategories,
