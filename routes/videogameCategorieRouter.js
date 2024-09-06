4f66606371c44b7ae6fdbb9ae711690d260355d5
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-06 19:55:28 +0200

    Add controller to route

--- routes/videogameCategorieRouter.js
+++ routes/videogameCategorieRouter.js
@@ -2,8 +2,8 @@ const express = require("express");
 const videogameCategorieController = require("../controllers/videogameCategorieController");
 const router = express.Router();
 
-router.get("/", videogameCategorieController.getAllVideogameCategory);
-router.get("/:id");
+router.get("/", videogameCategorieController.getAllVideogameCategories);
+router.get("/:id", videogameCategorieController.getVideogameCategorie);
 router.post("/:id");
 router.post("/updata");
 router.post("/delete");
