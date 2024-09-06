293e537ab098d9858f844bba8287cb60e8200cfe
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-06 19:54:43 +0200

    Add getVideogameCategorie controller

--- controllers/videogameCategorieController.js
+++ controllers/videogameCategorieController.js
@@ -1,12 +1,27 @@
 const db = require("../db/queries");
 
-async function getAllVideogameCategory(req, res) {
+async function getAllVideogameCategories(req, res) {
   let videogameCategories = await db.getAllVideogamesCategories();
   res.render("view_all_videogames_categories", {
     videogameCategories: videogameCategories,
   });
 }
 
+async function getVideogameCategorie(req, res) {
+  let videogameGenreInformation = await db.getVideogameCategorieDescription(
+    req.params.id
+  );
+  let videogameSpecificGenre = await db.getAllVideogamesWithSpecificGenre(
+    req.params.id
+  );
+
+  res.render("view_videogame_categorie", {
+    videogameGenreInformation: videogameGenreInformation,
+    videogameGenreList: videogameSpecificGenre,
+  });
+}
+
 module.exports = {
-  getAllVideogameCategory,
+  getAllVideogameCategories,
+  getVideogameCategorie,
 };
