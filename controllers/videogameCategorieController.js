39d35a10fa6c8eca2daea87569498515c0ad4fc1
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-09 19:36:39 +0200

    Add updateVideogameCategorie controller

--- controllers/videogameCategorieController.js
+++ controllers/videogameCategorieController.js
@@ -1,13 +1,14 @@
 const db = require("../db/queries");
 
-async function getAllVideogameCategories(req, res) {
+async function getAllVideogameCategoriesGet(req, res) {
   let videogameCategories = await db.getAllVideogamesCategories();
+
   res.render("view_all_videogames_categories", {
     videogameCategories: videogameCategories,
   });
 }
 
-async function getVideogameCategorie(req, res) {
+async function getVideogameCategorieGet(req, res) {
   let videogameGenreInformation = await db.getVideogameCategorieDescription(
     req.params.id
   );
@@ -18,10 +19,41 @@ async function getVideogameCategorie(req, res) {
   res.render("view_videogame_categorie", {
     videogameGenreInformation: videogameGenreInformation,
     videogameGenreList: videogameSpecificGenre,
+    videogameCategorieParams: req.params,
+  });
+}
+
+async function getUpdateVideogameCategorieGet(req, res) {
+  let videogameGenreInformation = await db.getVideogameCategorieDescription(
+    req.params.id
+  );
+
+  res.render("update_videogame_categorie", {
+    videogameGenreInformation: videogameGenreInformation,
+    videogameCategorieParams: req.params,
+  });
+}
+
+async function updateVideogameCategoriePost(req, res) {
+  let videogameCategorieInformations = [
+    req.body.videogameCategorie,
+    req.body.videogameCategorieDescription,
+    req.body.videogameCategorieImage,
+    req.params.videogameCategorieId,
+  ];
+
+  await db.updateVideogameCategorie(videogameCategorieInformations);
+  let videogameCategories = await db.getAllVideogamesCategories();
+
+  res.redirect("/videogame_categorie");
+  res.render("view_all_videogames_categories", {
+    videogameCategories: videogameCategories,
   });
 }
 
 module.exports = {
-  getAllVideogameCategories,
-  getVideogameCategorie,
+  getAllVideogameCategoriesGet,
+  getVideogameCategorieGet,
+  getUpdateVideogameCategorieGet,
+  updateVideogameCategoriePost,
 };
