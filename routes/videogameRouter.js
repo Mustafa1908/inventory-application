5e3e5a451313231539a0a19ff802c0211dbc9578
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-10 17:44:29 +0200

    Add router and controller for post and get for updateVideogame

--- routes/videogameRouter.js
+++ routes/videogameRouter.js
@@ -4,8 +4,7 @@ const router = express.Router();
 
 router.get("/", videogameController.getAllVideogamesGet);
 router.get("/:id", videogameController.getVideogameGet);
-router.post("/:id");
+router.get("/:id/update", videogameController.getUpdateVideogameGet);
 router.post("/updata");
-router.post("//delete");
 
 module.exports = router;
