46da277dcbeaa286043bbb1d07accc4b45f18ede
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-10 17:45:15 +0200

    Add get update videogame controller

--- controllers/videogameController.js
+++ controllers/videogameController.js
@@ -20,10 +20,45 @@ async function getVideogameGet(req, res) {
 
   res.render("view_videogame", {
     videogameInformation: videogameGenrePublisher,
+    videogameParams: req.params,
+  });
+}
+
+async function getUpdateVideogameGet(req, res) {
+  let allVideogameCategorie = await db.getAllVideogamesCategories();
+  let videogameInformation = await db.getVideogame(req.params.id);
+  let videogameGenre = splitStringInArray.splitStringIntoArray(
+    videogameInformation[0].videogame_genre
+  );
+  videogameInformation[0].videoGameGenreArray = videogameGenre;
+  let genreArray = [];
+
+  //Create genreArray
+  for (let i = 0; i < allVideogameCategorie.length; i++) {
+    genreArray.push(["false"]);
+  }
+
+  //Check which genre are checked
+  for (let i = 0; i < allVideogameCategorie.length; i++) {
+    if (
+      videogameInformation[0].videoGameGenreArray.includes(
+        allVideogameCategorie[i].videogame_categorie_name
+      )
+    ) {
+      genreArray[i] = ["true"];
+    }
+  }
+
+  res.render("update_videogame", {
+    videogameCategories: allVideogameCategorie,
+    videogameInformation: videogameInformation,
+    videogameCategorieParams: req.params,
+    videogameGenreChecked: genreArray,
   });
 }
 
 module.exports = {
   getAllVideogamesGet,
   getVideogameGet,
+  getUpdateVideogameGet,
 };
--- db/queries.js
+++ db/queries.js
@@ -51,6 +51,14 @@ async function updateVideogameCategorie(videogameCategorieInformations) {
   return rows;
 }
 
+async function getVideogame(videogameName) {
+  const { rows } = await pool.query(
+    `SELECT videogame.id, videogame_name,  videogame_description, videogame_price, videogame_image, videogame_release_date, videogame_rating, publisher, videogame_quantity,  STRING_AGG(videogame_categorie_name, ' ' ORDER BY videogame_categorie_name ASC) AS videogame_genre FROM videogame JOIN videogame_genre ON videogame.id = videogame_genre.id JOIN videogame_publisher ON videogame.id = videogame_publisher.id WHERE videogame_name = '${videogameName}'  GROUP BY videogame.id, videogame_genre.id, videogame_publisher.publisher;
+`
+  );
+  return rows;
+}
+
 async function getCurrentVideogameId() {
   const { rows } = await pool.query("SELECT id FROM videogame");
   return rows;
@@ -107,6 +115,7 @@ module.exports = {
   getAllVideogamesCategoriesAndDescriptions,
   getAllVideogamesCategories,
   updateVideogameCategorie,
+  getVideogame,
   getCurrentVideogameId,
   insertNewVideogame,
   insertNewVideogameGenre,
