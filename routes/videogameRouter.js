dd705265906bb782706508641f98aa1ec0d17971
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-11 19:29:08 +0200

    Add videogame id in routher paths, and add router and controllerr for update

--- routes/videogameRouter.js
+++ routes/videogameRouter.js
@@ -3,8 +3,14 @@ const videogameController = require("../controllers/videogameController");
 const router = express.Router();
 
 router.get("/", videogameController.getAllVideogamesGet);
-router.get("/:id", videogameController.getVideogameGet);
-router.get("/:id/update", videogameController.getUpdateVideogameGet);
-router.post("/updata");
+router.get("/:id/:videogameId", videogameController.getVideogameGet);
+router.get(
+  "/:id/:videogameId/update",
+  videogameController.getUpdateVideogameGet
+);
+router.post(
+  "/:id/:videogameId/update",
+  videogameController.updateVideogamePost
+);
 
 module.exports = router;
