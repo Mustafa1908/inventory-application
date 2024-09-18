b3a36634b91b93d341ce94886835f54aee6e9240
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-18 17:57:35 +0200

    Add errors messages, sanitazor for updateVideogamePost controlelr, re render update video game if error is not empty

--- controllers/videogameController.js
+++ controllers/videogameController.js
@@ -51,30 +51,110 @@ getUpdateVideogameGet = asyncHandler(async (req, res) => {
   });
 });
 
-updateVideogamePost = asyncHandler(async (req, res) => {
-  let uptadedVideogame = [
-    req.body.videogameName,
-    req.body.videogameDescription,
-    req.body.videogamePrice,
-    req.body.videogameImage,
-    req.body.videogameReleaseDate,
-    req.body.videogameRating,
-    req.body.videogameQuantity,
-    req.params.videogameId,
-  ];
+//Create new videogame form error messages
+const videogameNameMessageErrorMessage = "must be  between 1 and 80 characters";
+const videogameDescriptionErrorMessage =
+  "must be  between 30 and 300 characters";
+const videogamePublisherErrorMessage = "must be  between 3 and 120 characters";
+const videogamePriceErrorMessage = "must be a number";
+const videogameQuantityErrorMessage = "must be an integer(5, 7, 10, 13, ...)";
+const videogameRatingErrorMessage = "must be a number";
+const videogameDateErrorMessage = "must be a valid date";
+const videogameImageUrlErrorMessage = "must be a valid url";
+const videogameImageLengthErrorMessage = "must be  between 3 and 20 characters";
+const videogameGenreErrorMessage = "must be checked";
 
-  updateVideogameGenrePost(req);
-  updateVideogamePublisherPost(req);
-  await db.updateVideogame(uptadedVideogame);
-  res.redirect("/videogame");
+const validateVideogameMessage = [
+  body("videogameName")
+    .trim()
+    .isLength({ min: 1, max: 80 })
+    .withMessage(`Videogame  name ${videogameNameMessageErrorMessage}`),
+  body("videogameDescription")
+    .trim()
+    .isLength({ min: 30, max: 300 })
+    .withMessage(`Videogame  Description ${videogameDescriptionErrorMessage}`),
+  body("videogamePublisher")
+    .trim()
+    .isLength({ min: 3, max: 120 })
+    .withMessage(`Videogame  Publisher ${videogamePublisherErrorMessage}`),
+  body("videogameQuantity")
+    .trim()
+    .isNumeric()
+    .withMessage(`Videogame  Quantity ${videogameQuantityErrorMessage}`),
+  body("videogamePrice")
+    .trim()
+    .isFloat()
+    .withMessage(`Videogame  Price ${videogamePriceErrorMessage}`),
+  body("videogameRating")
+    .trim()
+    .isNumeric()
+    .withMessage(`Videogame  Rating ${videogameRatingErrorMessage}`),
+  body("videogameReleaseDate")
+    .trim()
+    .isDate()
+    .withMessage(`Videogame  Date ${videogameDateErrorMessage}`),
+  body("videogameImage")
+    .trim()
+    .isURL()
+    .withMessage(`Videogame categorie image ${videogameImageUrlErrorMessage}`)
+    .isLength({ min: 6, max: 280 })
+    .withMessage(
+      `Videogame categorie image ${videogameImageLengthErrorMessage}`
+    ),
+  body("videogameGenre")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`At least one videogame genre ${videogameGenreErrorMessage}`),
+];
 
-  let allVideogamesGenresPublishers =
-    await db.getAllVideogamesGenresPublishers();
+updateVideogamePost = [
+  validateVideogameMessage,
+  asyncHandler(async (req, res) => {
+    let uptadedVideogame = [
+      {
+        videogame_name: req.body.videogameName,
+        videogame_description: req.body.videogameDescription,
+        videogame_price: req.body.videogamePrice,
+        videogame_image: req.body.videogameImage,
+        videogamea_release_date: req.body.videogameReleaseDate,
+        videogame_rating: req.body.videogameRating,
+        videogame_quantity: req.body.videogameQuantity,
+        publisher: req.body.videogamePublisher,
+        videogame_id: req.params.videogameId,
+      },
+    ];
+    let allVideogameCategorie = await db.getAllVideogamesCategories();
+    let checkedOrNotArray =
+      await splitCheckedOrNotGenreInArray.splitCheckedOrNotGenreInArray(
+        req.params.id
+      );
+    const errors = validationResult(req);
 
-  res.render("view_all_videogames", {
-    videogameInformation: allVideogamesGenresPublishers,
-  });
-});
+    if (!errors.isEmpty()) {
+      console.log("hey");
+      return res.status(400).render("update_videogame", {
+        title: "Create videogame",
+        errors: errors.array(),
+        videogameCategories: allVideogameCategorie,
+        videogameInformation: uptadedVideogame,
+        videogameParams: req.params,
+        videogameGenreChecked: checkedOrNotArray,
+      });
+    }
+
+    updateVideogameGenrePost(req);
+    updateVideogamePublisherPost(req);
+    await db.updateVideogame(uptadedVideogame);
+    res.redirect("/videogame");
+
+    let allVideogamesGenresPublishers =
+      await db.getAllVideogamesGenresPublishers();
+
+    res.render("view_all_videogames", {
+      videogameInformation: allVideogamesGenresPublishers,
+    });
+  }),
+];
 
 updateVideogameGenrePost = asyncHandler(async (req) => {
   let allVideogamesCategoriesInformations =
