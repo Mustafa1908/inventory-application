f88aaac7ef211684d9172347bd4c4bda93d3cba2
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-09-28 13:04:09 +0200

    Remove host

--- app.js
+++ app.js
@@ -17,6 +17,4 @@ app.use("/videogame", videogameRouter);
 app.use("/videogame_categorie", videogameCategorieRouter);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Express app listening on port ${PORT}!`)
-);
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
